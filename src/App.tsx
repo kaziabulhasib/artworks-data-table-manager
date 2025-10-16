7ddc7c15232ec5e39aa2f67d5393e81a75de6804
kaziabulhasib <hasibkazi420@gmail.com>
2025-10-16 22:58:02 +0530

    init project & setup prime react

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { PrimeReactProvider } from "primereact/api";
+import { Button } from "primereact/button";
+
+import "primeicons/primeicons.css";
+
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+// import "primereact/resources/themes/arya-blue/theme.css";
+
+function App() {
+  return (
+    <PrimeReactProvider>
+      <Button label='Check' icon='pi pi-check' />
+    </PrimeReactProvider>
+  );
+}
+
+export default App;
